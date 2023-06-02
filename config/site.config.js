3750c702d45dd4c4132636bdd41a0209182bdf0e
C.Alvin Yao <ychx.alvin@gmail.com>
2023-06-02 17:03:31 +0900

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -45,16 +45,13 @@ module.exports = {
   protectedRoutes: ['/🌞 Private folder/u-need-a-password', '/🥟 Some test files/Protected route'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
-  email: 'mailto:null.medjed@gmail.com',
+  email: '',
 
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
   // you provide. See the documentation for details.
   links: [
-    {
-      name: 'Twitter',
-      link: 'https://twitter.com/AlvinSkyoh',
-    },
+    
   ],
 
   // This is a day.js-style datetime format string to format datetimes in the app. Ref to
